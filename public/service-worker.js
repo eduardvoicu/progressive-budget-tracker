d33856a6723b68db05c020f895a068e018ed7b88
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-11 07:34:40 -0600

    service-worker js file created

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/favicon.ico",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+  ];
+  
