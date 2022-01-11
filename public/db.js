a0b7e1ff37c96eebffac01f10c4681ead366a6f7
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-11 07:52:27 -0600

    finished function checkDatabase

--- public/db.js
+++ public/db.js
@@ -28,3 +28,25 @@ request.onsuccess = function(event) {
     const transaction = db.transaction(['pending'], 'readwrite');
     const store = transaction.objectStore('pending');
     const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+          fetch('/api/transaction/bulk', {
+            method: 'POST',
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+              'Content-Type': "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const store = transaction.objectStore('pending');
+          store.clear();
+        });
+      }
+    };
+  };
+  
+  window.addEventListener('online', checkDatabase);
